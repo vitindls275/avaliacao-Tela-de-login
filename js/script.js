24a4abe1f4d0946863224ea6491534472a0df84d
vitindls275 <Victorsouzaribeiro20@gmail.com>
2023-12-08 17:58:21 -0300

    v7

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 // confirm("Deseja receber notificações de promoçoes do nosso site? ")
+
+// serviços page
+$(".custom-carousel").owlCarousel({
+    autoWidth: true,
+    loop: true
+  });
+  $(document).ready(function () {
+    $(".custom-carousel .item").click(function () {
+      $(".custom-carousel .item").not($(this)).removeClass("active");
+      $(this).toggleClass("active");
+    });
+  });
+// fim serviços page  
